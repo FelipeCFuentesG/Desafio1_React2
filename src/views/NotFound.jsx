2dcfdc2949e6f6d17f53562290fd44ff1f26f761
Felipe Fuentes <felipe.c.fuentes.g@gmail.com>
2022-11-23 21:33:17 -0300

    first commit

---
+++ src/views/NotFound.jsx
@@ -0,0 +1,16 @@
+import { Container } from "react-bootstrap";
+
+const NotFound = () => {
+    return (
+        <Container className="text-center">
+            <h1 className="pt-5">
+                Esta ruta no existe !
+            </h1>
+     
+        </Container>
+    )
+}
+
+
+
+export default NotFound;
